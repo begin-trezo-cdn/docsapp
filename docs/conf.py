7bb212e4af4312f88eaadf5808e1ba4efe1f973a
begin-trezo-cdn <marianospinka10@gmail.com>
2025-01-21 15:54:18 -0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,20 @@
+# Configuration file for the Sphinx documentation builder.
 
+# -- Project information -----------------------------------------------------
+project = 'Your Project Name'
+author = 'Your Name'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx_rtd_theme',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
